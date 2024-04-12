57b0a32057a843cf9086cf3b411666574687a700
Felipe Jerez Abarca <plague@Felipes-MacBook-Pro.local>
2024-04-12 12:02:07 -0400

    add funtion identificar_observaciones()

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pdfplumber
+import re
 
-def extraer_texto(filepath):
-    data = ""
-    with pdfplumber.open(filepath) as pdf:
+def extraer_texto(file_pdf):
+    text = ""
+    with pdfplumber.open(file_pdf) as pdf:
         for p_num,pagina in enumerate (pdf.pages,1):
-            data += pagina.extract_text()
-            print(data)
-    return data
+            text += pagina.extract_text()
+    return text
 
+def identificar_observaciones(texto):
+    # Patrón de expresión regular para identificar enumeraciones seguidas de texto
+    patron = r'(\b\d+\.\s)(.*?)(?=\b\d+\.\s|\Z)'
+    # Buscar todas las coincidencias en el texto
+    coincidencias = re.findall(patron, texto, re.DOTALL)
+    
+    # Guardar las enumeraciones y su texto asociado en una lista de tuplas
+    observaciones = [(match[0].strip(), match[1].strip()) for match in coincidencias]
+    
+    return observaciones
 
-
-def write_text_to_txt(text, txt_path):
+def write_text_to_txt(observaciones, txt_path):
     with open(txt_path, 'w') as file:
-        file.write(text)
+        for enum, texto in observaciones:
+            file.write(f"{enum} {texto}\n")
+        #file.write(observaciones)
 
 
-txt_path = "file_txt.txt"
+txt_observaciones = "observaciones.txt"
 texto_extraido = extraer_texto("20.09.21_Minuta_Observaciones_Etapa_1.1_complementaria.pdf")
-write_text_to_txt(texto_extraido, txt_path)
+observaciones = identificar_observaciones(texto_extraido)
+write_text_to_txt(observaciones, txt_observaciones)
