44e0710dd5339cbde1a113332b84a4d56a4b0a24
Felipe Jerez Abarca <plague@Felipes-MacBook-Pro.local>
2024-04-19 11:09:55 -0400

    correction description for get the text

--- class.py
+++ class.py
@@ -9,7 +9,6 @@ class ObservationsForm(tk.Tk):
         self.title("Llenar Observaciones")
         self.observaciones = observaciones
         self.current_index = 0
-
         # Establecer el tamaño mínimo de la ventana
         self.minsize(600, 400)
 
@@ -36,10 +35,10 @@ class ObservationsForm(tk.Tk):
         self.title_entry = tk.Entry(self, textvariable=self.title_var)
         self.title_entry.grid(row=1, column=1, sticky=N+S+E+W)
 
-        # self.description_label = tk.Label(self, text="Description:")
-        # self.description_label.grid(row=2, column=0, sticky="w")
-        # self.description_entry = tk.Entry(self,textvariable=self.description_var, width=50)
-        # self.description_entry.grid(row=2, column=1, columnspan=2)
+        self.description_label = tk.Label(self, text="Description:")
+        self.description_label.grid(row=2, column=0, sticky="w")
+        self.description_entry = tk.Entry(self,textvariable=self.description_var, width=50)
+        self.description_entry.grid(row=2, column=1, columnspan=2)
 
         self.description_label = tk.Label(self, text="Description:")
         self.description_label.grid(row=2, column=0, sticky="w")
@@ -103,7 +102,10 @@ class ObservationsForm(tk.Tk):
     def fill(self):
         if (title := self.title_var.get()) and (type := self.type_var.get()) and (priority := self.priority_var.get()) and (status := self.status_var.get()):
             observation = list(self.observaciones[self.current_index])
+            description_text = self.description_entry.get('1.0',tk.END).strip()
+
             observation[1] = title
+            observation[2] = description_text
             observation[3] = type
             observation[4] = priority
             observation[5] = status
